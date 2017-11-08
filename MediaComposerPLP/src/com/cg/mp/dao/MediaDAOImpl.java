69cfd03648a0551dc9d08f25e27620f64cfc5446
Sahoo <pratik.sahoo@capgemini.com>
2017-11-08 16:50:02 +0530

    pushed by pratik

--- MediaComposerPLP/src/com/cg/mp/dao/MediaDAOImpl.java
+++ MediaComposerPLP/src/com/cg/mp/dao/MediaDAOImpl.java
@@ -2,9 +2,13 @@ package com.cg.mp.dao;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Repository;
+
+
+import com.cg.mp.dto.UserMasterDTO;
 
 
 @Repository
@@ -17,7 +21,13 @@ public class MediaDAOImpl implements IMediaDAO {
 	@Override
 	public int checkLogin(String username, String password) {
 		// TODO Auto-generated method stub
-		return 0;
+		
+		TypedQuery<UserMasterDTO> query = entityManager.createQuery("SELECT userMasterDTO FROM UserMasterDTO"
+				+ "userMasterDTO WHERE userMasterDTO.userId=:puserId and userMasterDTO.userPassword=:puserPassword", UserMasterDTO.class);
+		query.setParameter("puserId", username);
+		query.setParameter("puserPassword", password);
+		UserMasterDTO userMasterDTO = query.getSingleResult();
+		return userMasterDTO.getUserFlag();
 	}
 
 }
