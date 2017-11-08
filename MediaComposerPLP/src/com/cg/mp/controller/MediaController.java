563cf4711a0ffc82dd76030ca6c805602ef6096a
Sahoo <pratik.sahoo@capgemini.com>
2017-11-08 15:23:08 +0530

    pushed by pratik

--- MediaComposerPLP/src/com/cg/mp/controller/MediaController.java
+++ MediaComposerPLP/src/com/cg/mp/controller/MediaController.java
@@ -2,6 +2,9 @@ package com.cg.mp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.cg.mp.service.IMediaService;
 
@@ -12,5 +15,19 @@ public class MediaController {
 	
 	@Autowired
 	private IMediaService mediaService;
+	
+	@RequestMapping("/login.obj")
+	public String getMainPage(@RequestParam("username") String username,
+			@RequestParam("password") String password,
+			Model map){
+		String redirect;
+		
+		if(username.equals("bvk") && password.equalsIgnoreCase("bvk")){
+			redirect = new String("traineemgtsys");
+		}else{
+			redirect = new String("../../login");
+		}
+		return redirect;
+	}
 
 }
--- MediaComposerPLP/src/com/cg/mp/service/MediaService.java
+++ MediaComposerPLP/src/com/cg/mp/service/MediaService.java
@@ -1,5 +1,9 @@
 package com.cg.mp.service;
 
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class MediaService implements IMediaService {
 
 }
